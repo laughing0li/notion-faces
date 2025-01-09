3f4badf745bb78037ce0475b8c48c3a0d9d2ecc8
Yunlong Li <laughinglyl90@gmail.com>
2025-01-09 22:15:08 +0800

    add own ga

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,42 +1,41 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react'
 
 class Document extends NextDocument {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link href="/fonts/Quicksand.tff" as="font" crossOrigin="anonymous" />
-          {/* Google Analytics */}
-          {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3671095188342075"
-            crossOrigin="anonymous"
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link href="/fonts/Quicksand.tff" as="font" crossOrigin="anonymous" />
+                    {/* Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=G-064L7CLPEG`}
+                    />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3671095188342075"
+                        crossOrigin="anonymous"
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                     window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-064L7CLPEG');
           `,
-            }}
-          /> */}
-        </Head>
-        <body className="font-bold">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+                        }}
+                    />
+                </Head>
+                <body className="font-bold">
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
 
-export default Document;
+export default Document
