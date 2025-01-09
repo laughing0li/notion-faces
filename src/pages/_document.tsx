48d7aa95b3898c40bea96a07c793a4fca0a2127b
Yunlong Li <laughinglyl90@gmail.com>
2025-01-09 22:20:04 +0800

    add more analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,6 +28,18 @@ class Document extends NextDocument {
           `,
                         }}
                     />
+                    <script defer data-domain="notionfaces.net" src="https://actone.app/js/script.js"></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                     (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "pr8rlbakct");
+          `,
+                        }}
+                    />
                 </Head>
                 <body className="font-bold">
                     <Main />
